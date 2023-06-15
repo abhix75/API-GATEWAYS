a09c98647ccf72f7f3b7e6085124d8fac15bb3c6
Abhijit Mishra <abhijitmishraak10@gmail.com>
2023-06-15 13:17:58 +0530

    Added new user signup API

---
+++ src/controllers/user-controller.js
@@ -0,0 +1,32 @@
+const { StatusCodes }= require('http-status-codes');
+
+const { UserService} = require('../services/index');
+const user = require('../models/user');
+const { SuccessResponse, ErrorResponse } = require('../utils/common');
+
+
+
+
+async function signup(req,res)
+{
+     try {
+        const user = await UserService.create({
+            email: req.body.email,
+            password: req.body.password
+        });
+        SuccessResponse.data = user;
+        return res
+                 .status(StatusCodes.CREATED)
+                 .json(SuccessResponse)
+    } catch (error)
+     {
+        ErrorResponse.error=error;
+        return res
+                 .status(error.statusCodes)
+                 .json(ErrorResponse)
+     }
+ }
+
+ module.exports={
+    signup
+ }
