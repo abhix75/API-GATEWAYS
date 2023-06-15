bf3f568f5af8b9bacc549b6613a6e5fa546396ce
Abhijit Mishra <abhijitmishraak10@gmail.com>
2023-06-15 17:29:02 +0530

    ADDED SIGNIN WITH JWT

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -26,7 +26,28 @@ async function signup(req,res)
                  .json(ErrorResponse)
      }
  }
+ async function signin(req,res)
+{
+     try {
+        const user = await UserService.signin({
+            email: req.body.email,
+            password: req.body.password
+        });
+        SuccessResponse.data = user;
+        return res
+                 .status(StatusCodes.CREATED)
+                 .json(SuccessResponse)
+    } catch (error)
+     {  console.log(error);
+        ErrorResponse.error=error;
+        return res
+                 .status(error.statusCodes)
+                 .json(ErrorResponse)
+     }
+ }
+
 
  module.exports={
-    signup
+    signup,
+    signin
  }
--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,6 +1,7 @@
 const {StatusCodes}=require("http-status-codes")
 const { UserRepository } = require('../repositories');
 const AppError = require('../utils/error/app-error');
+const {Auth}=require('../utils/common');
 const userRepository = new UserRepository();
 
 
@@ -25,7 +26,29 @@ async function create(data)
         throw new AppError("cannot create new user object",StatusCodes.INTERNAL_SERVER_ERROR);
     }
 }
+async function signin(data) {
+    try {
+        const user = await userRepository.getUserByEmail(data.email);
+        if(!user){
+            throw new AppError('No User Found For This EMAIL',StatusCodes.NOT_FOUND);
+        }
+        const passwordMatch=Auth.checkPassword(data.password,user.password);
+        console.log("Password Match",passwordMatch);
+        if(!passwordMatch){
+            throw new AppError("INVALID PASSWORD",StatusCodes.BAD_REQUEST);
+        }
+
+        const jwt = Auth.createToken({id:user.id,email:user.email});
+        return jwt;
+    } catch (error) {
+          if(error instanceof AppError) throw error;
+          console.log(error);
+          throw new AppError('Something went wrong',StatusCodes.INTERNAL_SERVER_ERROR);
+
+    }
+}
 
 module.exports={
-    create
+    create,
+    signin
 }
