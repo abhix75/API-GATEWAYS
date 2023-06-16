d633c39094dab7f521c086e13c58844b5dc07aae
Abhijit Mishra <abhijitmishraak10@gmail.com>
2023-06-16 19:29:01 +0530

    added disauthenticated middleware

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const {Auth}=require('./utils/common')
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
 
@@ -11,7 +10,5 @@ app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
-    const token ="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTMsImVtYWlsIjoia2lybnJpQGdtYWlsLmNvbSIsImlhdCI6MTY4NjkyMTA5NCwiZXhwIjoxNjg2OTI0Njk0fQ.xBc2qD7EOae4P3VT0eAwI4cSa8N0PYgefwyb0XagkO4"
-    const response = Auth.verifyToken(token);
-    console.log(response);
+   
 });
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
 const { InfoController } = require('../../controllers');
-
+const{AuthRequestMiddleWares}=require('../../middlewares')
 const userRoutes= require('./user-routes.js')
 const router = express.Router();
 
-router.get('/info',
+router.get('/info',AuthRequestMiddleWares.checkAuth,
                   InfoController.info);
 
 router.use('/user', userRoutes);
