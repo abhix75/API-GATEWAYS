802a7ccf718d8bb2640ecf94d3312359ec14d224
Abhijit Mishra <abhijitmishraak10@gmail.com>
2023-07-02 15:44:18 +0530

    Added rate limiter and proxy

--- src/config/server-config.js
+++ src/config/server-config.js
@@ -6,5 +6,7 @@ module.exports = {
     PORT: process.env.PORT,
     SALT_ROUND:process.env.SALT_ROUND,
     JWT_SECRET:process.env.JWT_SECRET,
-    JWT_EXPIRY:process.env.JWT_EXPIRY
+    JWT_EXPIRY:process.env.JWT_EXPIRY,
+    Flight_Service:process.env.Flight_Service,
+    booking_Service:process.env.booking_Service
 }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
+const rateLimit = require('express-rate-limit')
+const {createProxyMiddleware}=require('http-proxy-middleware')
 
 const app = express();
+const limiter = rateLimit({
+	windowMs: 2 * 60 * 1000, // 2 minutes
+	max: 3, // Limit each IP to 3 requests per `window` (here, per 2 minutes)
+
+})
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+// Apply the rate limiting middleware to all requests
+app.use(limiter);
+
+app.use('/flightsService', createProxyMiddleware({ target:ServerConfig.Flight_Service, changeOrigin: true }));
+app.use('/bookingService',createProxyMiddleware({ target:ServerConfig.booking_Service, changeOrigin: true }));
 app.use('/api', apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
